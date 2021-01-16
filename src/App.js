3087186382092daf7cd8da954bb2581a695601be
vvisionnn <wz546500392@gmail.com>
2021-01-16 17:25:00 +0800

    finish basic converting function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const parseTorrent = require("parse-torrent");
 
 function App() {
+  const [magnetList, setMagnetList] = React.useState([]);
+
+  const handleFileChosen = async (event) => {
+    const files = event.target.files;
+
+    const readFileAsync = (file) =>
+      new Promise((resolve) => {
+        var reader = new FileReader();
+        reader.onload = function () {
+          var arrayBuffer = reader.result;
+          const bf = Buffer.from(arrayBuffer);
+          const torrentFile = parseTorrent(bf);
+          const magnetLink = `magnet:?xt=urn:btih:${torrentFile.infoHash}&dn=${torrentFile.name}`;
+          resolve(magnetLink);
+        };
+        reader.readAsArrayBuffer(file);
+      });
+
+    let tempList = [];
+    for (let i = 0; i < files.length; i++) {
+      tempList.push(await readFileAsync(files[i]));
+    }
+    setMagnetList(tempList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="file"
+        accept=".torrent"
+        onChange={handleFileChosen}
+        multiple
+      />
+      {magnetList.length > 0 && (
+        <ul>
+          {magnetList.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      )}
+    </>
   );
 }
 
