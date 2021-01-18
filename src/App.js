d3666cdad47f51a672c6fbb29b92535f91a4217f
vvisionnn <wz546500392@gmail.com>
2021-01-18 17:49:50 +0800

    update style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
 import "./App.css";
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  createMuiTheme,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 const parseTorrent = require("parse-torrent");
+
+const customTheme = createMuiTheme({});
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "white",
+    color: "black",
+    flexGrow: 1,
+  },
+  appbarTitle: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
   const [magnetList, setMagnetList] = React.useState([]);
+  const classes = useStyle();
 
   const handleFileChosen = async (event) => {
     const files = event.target.files;
@@ -29,21 +54,34 @@ function App() {
   };
 
   return (
-    <>
-      <input
-        type="file"
-        accept=".torrent"
-        onChange={handleFileChosen}
-        multiple
-      />
-      {magnetList.length > 0 && (
-        <ul>
-          {magnetList.map((item, index) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
-      )}
-    </>
+    <div className={classes.root}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <AppBar className={classes.appbar}>
+          <Toolbar variant={"dense"}>
+            <Typography variant="h6" className={classes.appbarTitle}>
+              Title
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Toolbar variant={"dense"} />
+          <input
+            type="file"
+            accept=".torrent"
+            onChange={handleFileChosen}
+            multiple
+          />
+          {magnetList.length > 0 && (
+            <ul>
+              {magnetList.map((item, index) => {
+                return <li key={index}>{item}</li>;
+              })}
+            </ul>
+          )}
+        </main>
+      </ThemeProvider>
+    </div>
   );
 }
 
